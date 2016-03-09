0cead1e9f9997fa37ddc918957837dc2b99526f9
Rabea Gleissner <rabeagleissner@gmail.com>
2016-03-09 13:01:57 +0000

    prints a multiplication statement to the console

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -4,4 +4,8 @@ class MultiplicationTable
     factor1 * factor2
   end
 
+  def print_statement(factor1, factor2, result)
+    puts "#{factor1} x #{factor2} = #{result}"
+  end
+
 end
--- spec/multiplication_table_spec.rb
+++ spec/multiplication_table_spec.rb
@@ -3,8 +3,13 @@ require_relative '../lib/multiplication_table'
 
 describe MultiplicationTable do
 
+  let(:table) {MultiplicationTable.new}
+
   it "multiplies two given numbers" do
-    table = MultiplicationTable.new
     expect(table.multiply(2,3)).to eq(6)
+  end
+
+  it "prints multiplication statement to console" do
+    expect{table.print_statement(2,3,6)}.to output("2 x 3 = 6\n").to_stdout
   end
 end
